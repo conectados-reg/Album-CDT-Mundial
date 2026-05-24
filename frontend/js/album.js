8b08414de966f14c848fce3cb1abc22965f67cf6
conectados-reg <conectados@sportline.com.pa>
2026-05-24 00:58:44 -0500

    Update album.js

--- frontend/js/album.js
+++ frontend/js/album.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/api";
+const API_URL = "https://album-cdt-mundial.onrender.com/api";
 
 document.addEventListener('DOMContentLoaded', async () => {
   const session = requireAuth();
